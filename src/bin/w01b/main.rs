a9fc53e6e88d32d5fe4c023bafecb9f4f2adcb75
Vincent Alec F. Baguio <vfbaguio@up.edu.ph>
2024-02-20 13:34:39 +0800

    finish b and start c

--- src/bin/w01b/main.rs
+++ src/bin/w01b/main.rs
@@ -1,32 +1,126 @@
-use std::io;
+use core::panic;
+use std::env;
+use std::fs::{write, File};
+use std::i64;
+use std::io::{self, BufRead};
+use std::path::Path;
 
 fn main() {
-    let mut str_in = String::new();
+    let args: Vec<String> = env::args().collect();
+    let config = Config::new(&args);
+    println!("Input path: {}", config.input_path);
+    let mut taylor_series = TaylorSeries::new();
 
-    io::stdin().read_line(&mut str_in)
-        .expect("Invalid input!");
+    if let Ok(lines) = read_lines(config.input_path) {
+        let rows = lines.flatten().collect::<Vec<String>>();
+        let mut output_string = String::new();
+        let mut n: usize = 0;
 
-    let n_testcases: u64 = str_in.trim().parse().expect("Not an integer!"); 
+        for (index, line) in rows.iter().enumerate() {
+            if index == 0 {
+                n = line.parse().unwrap();
+                continue;
+            // ensure to use next n-lines
+            } else if index <= n {
+                let text: String = line.parse().unwrap();
+                let values: Vec<&str> = text.split(" ").collect();
+                let (fn_type, r, x) = (
+                    values[0].chars().next().unwrap_or('\0'),
+                    values[1].parse::<f64>(),
+                    values[2].parse::<f64>(),
+                );
 
-    for _ in 1..n_testcases + 1{
-        str_in.clear();
-        io::stdin().read_line(&mut str_in)
-            .expect("Invalid input!");
+                output_string.push_str(&format!(
+                    "Function #{}: {:.2}\n",
+                    index,
+                    taylor_series.estimate(fn_type, x.unwrap(), r.unwrap())
+                ));
+            }
+        }
 
-        let split_in: Vec <&str> = str_in.splitn(3, ' ').collect();
+        write_to_output(config.output_path, output_string);
+    }
+}
 
-        let cmd = split_in[0];
-        let r_desired: f64 = split_in[1].trim().parse().expect("Not a float!");
-        let x: f64 = split_in[2].trim().parse().expect("Not a number!");
-        
-        let mut temp: f64 = 0.0;
-        let mut factorial: f64 = 1.0; 
-        let mut n: f64 = 0.0; 
+fn write_to_output(path: String, data: String) {
+    write(path, data).expect("Error writing to file");
+}
 
-        // TODO: Find the desired number of iterations n. Make sure to get its
-        //       floored value
+struct TaylorSeries {}
 
-        // TODO: ... then compute the Taylor approximation of a function
-        //       depending on the command.
+impl TaylorSeries {
+    fn new() -> TaylorSeries {
+        TaylorSeries {}
+    }
+
+    fn natural_exponent(&mut self, x: f64, n: i64) -> f64 {
+        f64::powf(x, n as f64) / factorial(n)
+    }
+
+    fn sin(&mut self, x: f64, n: i64) -> f64 {
+        f64::powf(x, (2 * n + 1) as f64) * f64::powf(-1.0, n as f64) / factorial(2 * n + 1)
+    }
+
+    fn cos(&mut self, x: f64, n: i64) -> f64 {
+        f64::powf(x, (2 * n) as f64) * f64::powf(-1.0, n as f64) / factorial(2 * n)
+    }
+
+    fn estimate(&mut self, fn_type: char, x: f64, r: f64) -> f64 {
+        let mut n = 0;
+        let mut sum: f64 = 0.0;
+
+        // From constraints
+        while n < 21 {
+            let value: f64 = match fn_type {
+                'e' => self.natural_exponent(x, n),
+                's' => self.sin(x, n),
+                'c' => self.cos(x, n),
+                _ => panic!("Function type was not indicated"),
+            };
+
+            sum += value;
+
+            if value.abs() <= r {
+                return sum;
+            }
+
+            n += 1;
+        }
+        sum
+    }
+}
+
+fn factorial(n: i64) -> f64 {
+    match n {
+        0 => 1.0,
+        _ => (1..=n).map(|x| x as f64).product(),
+    }
+}
+
+// https://doc.rust-lang.org/rust-by-example/std_misc/file/read_lines.html
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
+// https://doc.rust-lang.org/book/ch12-03-improving-error-handling-and-modularity.html#creating-a-constructor-for-config
+struct Config {
+    input_path: String,
+    output_path: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        // we can add error handling here next time
+        let input_path = args[1].clone();
+        let output_path = args[2].clone();
+
+        Config {
+            input_path,
+            output_path,
+        }
     }
 }
--- src/bin/w01c/main.rs
+++ src/bin/w01c/main.rs
@@ -1,14 +1,39 @@
 use std::io;
 
+// prefix length 4
+// last_digit length 1
 fn get_network_from_prefix(prefix: u64, last_digit: u64) -> u64 {
-    // TODO: Write a routine that will output the following numbers
-    //       given the prefix and the last digit (i.e. first digit
-    //       of the unique id)
-    // 0 - Invalid prefix
-    // 1 - Globe/TM
-    // 2 - Smart/Sun/TNT
-    // 3 - DITO
-    0
+    let globe_tm: [u64; 27] = [ 817, 905, 906, 915, 916, 917, 926, 927, 935, 936, 937, 945, 953, 954,
+        955, 956, 965, 966, 967, 975, 976, 977, 978, 979, 995, 996, 997 ];
+    let globe_tm_long: [u64; 9] = [
+        9173, 9175, 9176, 9178, 9253, 9255, 9256, 9257, 9258
+    ];
+    let smart_sun_tnt: [u64; 38] = [
+        922, 923, 924, 925, 931, 932, 933, 934, 940, 941, 942, 943, 973,
+        974, 907, 909, 910, 912, 930, 938, 946, 948, 950, 908, 918, 919,
+        920, 921, 928, 929, 939, 946, 947, 949, 951, 961, 998, 999
+    ];
+    let dito: [u64; 8] = [
+        895, 896, 897, 898, 991, 992, 993, 994
+    ];
+
+    if dito.contains(&prefix) {
+        return 3;
+    } 
+
+    if smart_sun_tnt.contains(&prefix) {
+        return 2; 
+    } 
+
+    if globe_tm.contains(&prefix) {
+        return 1;
+    } 
+
+    if globe_tm_long.contains(&(last_digit+prefix*10)) {
+        return 1;
+    }
+
+    return 0;
 }
 
 fn main() {
@@ -32,6 +57,10 @@ fn main() {
         let phone_as_int: u64 = str_in.trim()
             .parse()
             .expect("Input is not an integer!");
+
+        let phone_as_str: String = phone_as_int.to_string();
+
+        println!("{}", phone_as_int)
 
         // TODO: Write a routine that will print out the mobile number and
         //       network provider according to the specs. Don't forget to use
