29399253e0dacd3ea8bb927ab74c259adaf94f2e
Vincent Alec F. Baguio <vfbaguio@up.edu.ph>
2024-02-28 20:45:51 +0800

    week 2a temporary

--- src/bin/w02a/main.rs
+++ src/bin/w02a/main.rs
@@ -6,33 +6,6 @@ struct Player {
     item: Option<PlayerItem>
 }
 
-impl Player {
-    fn new(name: String, item: Option<PlayerItem>) -> Self {}
-
-    fn action(&self, action_type: PlayerAction) -> String {
-        match action_type {
-            PlayerAction::left => {
-                self.pos -= 1;
-                format!("New position: {}", self.pos)
-            }
-            PlayerAction::right => {
-
-                self.pos += 1;
-                format!("New position: {}", self.pos)
-            }
-            PlayerAction::uitem => {
-                self.pos -= 1;
-                let item = self.item.
-                // check if item exists
-                format!(" ")
-            }
-            _ => format!("Error has occured")
-        }
-    }
-
-
-}
-
 struct PlayerItem {
     name: String,
     item_type: PlayerItemQtyType,
@@ -49,6 +22,52 @@ enum PlayerAction {
     uitem,
 }
 
+impl Player {
+    fn new(name: String, item: Option<PlayerItem>) -> Self {
+        Self {
+            name,
+            item,
+            pos: 0,
+        }
+    }
+
+    fn action(mut self, action_type: PlayerAction) -> String {
+        match action_type {
+            PlayerAction::left => {
+                self.pos -= 1;
+                format!("New position: {}", self.pos)
+            }
+            PlayerAction::right => {
+
+                self.pos += 1;
+                format!("New position: {}", self.pos)
+            }
+            PlayerAction::uitem => {
+                let item: PlayerItem = self.item.unwrap();
+
+                match item.item_type { 
+                    PlayerItemQtyType::Consumable(mut count) => {
+                        count -= 1;
+                        if count < 1 {
+                            self.item = None;
+                            format!("Player used <{}>. It is now gone", item.name)
+                        } else {
+                            format!("Player used <{}>. {}x of <{}> remains.", item.name, count, item.name)
+                        } 
+                    }
+                    PlayerItemQtyType::Once => {
+                        self.item = None;
+                        format!("Player used <{}>. It is now gone", item.name)
+                    }
+                }
+            }
+        }
+    }
+
+
+}
+
+
 fn main() {
     let mut str_in = String::new();
 
@@ -62,18 +81,14 @@ fn main() {
         io::stdin().read_line(&mut str_in).expect("Invalid input!");
 
         let player_info: Vec<&str> = str_in.split_whitespace().collect();
+        let item_amount = player_info[2].parse::<u64>().unwrap();
 
-        let item_amount = player_info[2].parse::<i64>();
-
-        let mut player = Player {
-            name: player_info[0].to_string(),
-            pos: 0,
-            item: PlayerItem {
-                name: player_info[1].to_string(),
-                item_type: if item_amount > 1 {PlayerItemQtyType::Consumable(item_amount)} else {PlayerItemQtyType::Once},
-            },
+        let item = PlayerItem {
+            name: player_info[1].to_string(),
+            item_type: if item_amount == 1 {PlayerItemQtyType::Once} else {PlayerItemQtyType::Consumable(item_amount)},
         };
 
+        let mut player = Player::new(player_info[0].to_string(), Some(item));
 
 
 
