de2d887da54572c4188701ec068686ccf6e28ea0
Vincent Alec F. Baguio <vfbaguio@up.edu.ph>
2024-03-14 00:11:11 +0800

    use template file w3c

--- src/bin/w03c/main.rs
+++ src/bin/w03c/main.rs
@@ -1,135 +1,6 @@
-use core::fmt;
-use std::{borrow::Borrow, collections::HashMap, fmt::write, io, num::NonZeroI8};
-
-fn main() {
-    let mut str_in = String::new();
-
-    io::stdin().read_line(&mut str_in).expect("Invalid input!");
-
-    let num_borrowers: u64 = str_in.trim().parse().expect("Not an integer!");
-
-    let mut borrowers_hash: HashMap<String, Borrower>= HashMap::new();
-
-    for _i in 1..=num_borrowers {
-        str_in.clear();
-        io::stdin().read_line(&mut str_in).expect("Invalid input!");
-
-        let date_borrower: Vec<_> = str_in.trim().splitn(2, ' ').collect();
-        
-        let name = date_borrower[1].to_string();
-
-        let dates: Vec<_> = date_borrower[0].trim().split("-").collect();
-        let year: u64 = dates[0].parse().expect("Failed to parse u64");
-        let month: u8 = dates[1].parse().expect("Failed to parse u8");
-        let day: u8 = dates[2].parse().expect("Failed to parse u8");
-
-        let reg_date = SplitDate{
-            year,
-            month,
-            day
-        };
-
-        let borrower = Borrower { name, reg_date };
-
-        borrowers_hash.insert(name, borrower);
-    }
-
-    str_in.clear();
-    io::stdin().read_line(&mut str_in).expect("Invalid input!");
-    let num_items: u64 = str_in.trim().parse().expect("Not an integer!");
-
-    let mut lent_items: Vec<LentItem> = Vec::new();
-    let mut lent_items_hash: HashMap<String, LentItem>= HashMap::new();
-
-    for _i in 1..=num_items {
-        str_in.clear();
-        io::stdin().read_line(&mut str_in).expect("Invalid input!");
-
-        let date_item: Vec<_> = str_in.trim().splitn(2, ' ').collect();
-        
-        let dates: Vec<_> = date_item[0].trim().split("-").collect();
-        let year: u64 = dates[0].parse().expect("Failed to parse u64");
-        let month: u8 = dates[1].parse().expect("Failed to parse u8");
-        let day: u8 = dates[2].parse().expect("Failed to parse u8");
-
-        let name = date_item[1].to_string();
-
-        let item = LentItem::new(name, year, month, day);
-
-        lent_items.push(item);
-        lent_items_hash.insert(name, item);
-    }
-
-    let lent_items_references: Vec<&LentItem> = lent_items.iter().collect();
-
-    str_in.clear();
-    io::stdin().read_line(&mut str_in).expect("Invalid input!");
-    let num_commands: u64 = str_in.trim().parse().expect("Not an integer!");
-
-    for _i in 1..=num_commands {
-        str_in.clear();
-        io::stdin().read_line(&mut str_in).expect("Invalid input!");
-
-        let commands: Vec<&str> = str_in.trim().split(' ').collect();
-
-        let cmd = commands[0];
-
-        if commands.len() == 2 {
-            let b_name = commands[1];
-            match cmd {
-                "bi" => {
-                    match borrowers_hash.get(b_name) {
-                        Some(person) => {
-                            println!("[BINFO] {}", person);
-
-                            let borrowed_items = person.borrowed_items(&lent_items_references);
-
-                            if !borrowed_items.is_empty() {
-                                println!("-----BORROWED ITEMS-----"); 
-                                for item in borrowed_items {
-                                    println!("{}", item);
-                                }
-                            }
-                        },
-                        None => println!("[BINFO] Borrower \"{}\" not found!", b_name)
-                    };
-                }, 
-                "ii" =>println!("{}", commands[1]),
-                "u" => println!("{}", commands[1]),
-                _ => unreachable!(),
-            }
-        } else if commands.len() == 3 {
-            let i_name = commands[1];
-            let b_name = commands[2];
-            match cmd {
-                "b" => {
-                    let borrower = borrowers_hash.get(b_name);
-
-                    if borrower.is_none() {
-                        println!("[BORROW] Borrower \"{}\" not found!", b_name)
-                    }
-
-                    
-
-                    let mut item = lent_items_hash.get(i_name);
-
-                    if item.is_none() {
-                        println!("[BORROW] Item \"{}\" not found!", i_name);
-                    }
-
-                    LentItem::borrow(item, &borrower);
-                },
-                _ => unreachable!(),
-            }
-        } else if commands.len() == 4 {
-            match cmd {
-                "t" => println!("{}", commands[1]),
-                _ => unreachable!(),
-                
-            }
-        }
-    }
-}
+use std::collections::HashMap;
+use std::fmt;
+use std::io;
 
 struct SplitDate {
     year: u64,
@@ -142,27 +13,13 @@ struct Borrower {
     reg_date: SplitDate,
 }
 
-struct LentItem<'a> {
+struct LentItem {
     name: String,
     acquire_date: SplitDate,
-    borrowed_by: Option<&'a Borrower>,
+    borrowed_by: Option<Borrower>,
 }
 
-// impl SplitDate {
-//     fn new(input: &str) -> Option<SplitDate> {
-//         let parts: Vec<&str> = input.trim().split('-').collect();
-//         if let (Ok(year), Ok(month), Ok(day)) = (
-//             parts[0].parse::<u64>(),
-//             parts[1].parse::<u8>(),
-//             parts[2].parse::<u8>(),
-//         ) {
-//             return Some(SplitDate { year, month, day });
-//         }
-//         None
-//     }
-// }
-
-impl<'a> LentItem<'a> {
+impl LentItem {
     fn new(name: String, year: u64, month: u8, day: u8) -> Self {
         Self {
             name,
@@ -171,34 +28,40 @@ impl<'a> LentItem<'a> {
         }
     }
 
-    fn borrow(&'a mut self, to: &'a Borrower) -> Option<&'a Borrower> {
+    fn borrow(&mut self, to: &Borrower) -> Option<&Borrower> {
         if self.borrowed_by.is_none() {
-            self.borrowed_by = Some(to);
+            self.borrowed_by = Some(Borrower::new(
+                to.name.clone(),
+                to.reg_date.year,
+                to.reg_date.month,
+                to.reg_date.day,
+            ));
             None
         } else {
-            self.borrowed_by
+            self.borrowed_by.as_ref()
         }
     }
 
     fn unborrow(&mut self) -> Option<&Borrower> {
-        if self.borrowed_by.is_none() {
-            None
-        } else {
-            return self.borrowed_by;
-        }
+        self.borrowed_by.as_ref().take()
     }
 
-    fn transfer(
-        &mut self,
+    fn transfer<'a>(
+        &'a mut self,
         from: &'a Borrower,
         to: &'a Borrower,
-    ) -> (Option<&Borrower>, Option<&Borrower>) {
+    ) -> (Option<&'a Borrower>, Option<&'a Borrower>) {
         let current_borrower_name = &self.borrowed_by.as_ref().unwrap().name;
         if from.name != *current_borrower_name || to.name == *current_borrower_name {
             return (Some(from), None);
         }
 
-        self.borrowed_by = Some(to);
+        self.borrowed_by = Some(Borrower::new(
+            to.name.clone(),
+            to.reg_date.year,
+            to.reg_date.month,
+            to.reg_date.day,
+        ));
         return (Some(from), Some(to));
     }
 }
@@ -211,29 +74,31 @@ impl Borrower {
         }
     }
 
-    fn borrowed_items<'a>(&self, items: &'a Vec<&'a LentItem<'a>>) -> Vec<&'a LentItem<'a>> {
-        let mut borrowed: Vec<&LentItem> = Vec::new();
+    fn borrowed_items<'a>(&self, items: &'a Vec<&LentItem>) -> Vec<&'a LentItem> {
+        let mut borrowed: Vec<&'a LentItem> = Vec::new();
         for item in items {
-            if let Some(borrowed_by) = item.borrowed_by {
+            if let Some(borrowed_by) = &item.borrowed_by {
                 if self.name == borrowed_by.name {
-                    borrowed.push(item);
+                    borrowed.push(*item);
                 }
             }
         }
 
+        borrowed.sort_by_key(|item| &item.name);
         borrowed
     }
 }
 
-impl fmt::Display for SplitDate {
-    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
-        write!(fmt, "{:04}-{:02}-{:02}", self.year, self.month, self.day)
+// This has been implemented for you as an example
+impl fmt::Display for Borrower {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Borrower({}) [Registered {}]", self.name, self.reg_date)
     }
 }
 
-impl<'a> fmt::Display for LentItem<'a> {
+impl fmt::Display for LentItem {
     fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
-        if let Some(borrowed_by) = self.borrowed_by {
+        if let Some(borrowed_by) = &self.borrowed_by {
             write!(
                 fmt,
                 "LentItem({}) [Acquired {}] [Borrowed by {}]",
@@ -249,12 +114,203 @@ impl<'a> fmt::Display for LentItem<'a> {
     }
 }
 
-impl std::fmt::Display for Borrower {
-    fn fmt(&self, fmt: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(
-            fmt,
-            "Borrower({}) [Registered {}]",
-            self.name, self.reg_date
-        )
+impl fmt::Display for SplitDate {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
+        write!(fmt, "{:04}-{:02}-{:02}", self.year, self.month, self.day)
+    }
+}
+
+// No need to edit starting from this line!
+fn main() {
+    let mut str_in = String::new();
+    let mut borrower_list: HashMap<String, Borrower> = HashMap::new();
+    let mut items_list: HashMap<String, LentItem> = HashMap::new();
+
+    io::stdin().read_line(&mut str_in).expect("Invalid input!");
+
+    let n_borrowers: usize = str_in.trim().parse().expect("Invalid number!");
+
+    for _ in 0..n_borrowers {
+        str_in.clear();
+        io::stdin().read_line(&mut str_in).expect("Invalid input!");
+
+        let split_in: Vec<&str> = str_in.splitn(2, ' ').collect();
+        let split_date: Vec<&str> = split_in[0].trim().splitn(3, '-').collect();
+
+        borrower_list.insert(
+            split_in[1].trim().to_string(),
+            Borrower::new(
+                split_in[1].trim().to_string(),
+                split_date[0].parse::<u64>().expect(""),
+                split_date[1].parse::<u8>().expect(""),
+                split_date[2].parse::<u8>().expect(""),
+            ),
+        );
+    }
+
+    str_in.clear();
+    io::stdin().read_line(&mut str_in).expect("Invalid input!");
+
+    let n_items: usize = str_in.trim().parse().expect("Invalid number!");
+
+    for _ in 0..n_items {
+        str_in.clear();
+        io::stdin().read_line(&mut str_in).expect("Invalid input!");
+
+        let split_in: Vec<&str> = str_in.splitn(2, ' ').collect();
+        let split_date: Vec<&str> = split_in[0].trim().splitn(3, '-').collect();
+
+        items_list.insert(
+            split_in[1].trim().to_string(),
+            LentItem::new(
+                split_in[1].trim().to_string(),
+                split_date[0].parse::<u64>().expect(""),
+                split_date[1].parse::<u8>().expect(""),
+                split_date[2].parse::<u8>().expect(""),
+            ),
+        );
+    }
+
+    str_in.clear();
+    io::stdin().read_line(&mut str_in).expect("Invalid input!");
+
+    let n_cmd: usize = str_in.trim().parse().expect("Invalid number!");
+
+    for _ in 0..n_cmd {
+        str_in.clear();
+        io::stdin().read_line(&mut str_in).expect("Invalid input!");
+
+        let split_in: Vec<&str> = str_in.splitn(2, ' ').collect();
+        let cmd: &str = split_in[0].trim();
+        let subcmd: String = split_in[1].trim().to_string();
+
+        match cmd {
+            "bi" => {
+                if let Some(borrower) = borrower_list.get(&subcmd) {
+                    println!("[BINFO] {}", borrower);
+                    println!("    -----BORROWED ITEMS-----");
+
+                    let items_list_vals = items_list.values().collect();
+                    let borrowed_items_list = borrower.borrowed_items(&items_list_vals);
+
+                    if borrowed_items_list.is_empty() {
+                        println!("    NONE");
+                    } else {
+                        for v in &borrowed_items_list {
+                            println!("    {}", v);
+                        }
+                    }
+                } else {
+                    println!("[BINFO] Borrower \"{}\" not found!", subcmd);
+                }
+            }
+            "ii" => {
+                if let Some(item) = items_list.get(&subcmd) {
+                    println!("[IINFO] {}", item);
+                } else {
+                    println!("[IINFO] Item \"{}\" not found!", subcmd);
+                }
+            }
+            "t" => {
+                let split_subcmd: Vec<&str> = subcmd.splitn(3, ' ').collect();
+                let item_t: String = split_subcmd[0].trim().to_string();
+                let from_t: String = split_subcmd[1].trim().to_string();
+                let to_t: String = split_subcmd[2].trim().to_string();
+
+                if let None = items_list.get(&item_t) {
+                    println!("[TRANSFER] Item \"{}\" not found!", item_t);
+                    continue;
+                }
+
+                if let None = borrower_list.get(&from_t) {
+                    println!("[TRANSFER] FROM borrower \"{}\" not found!", from_t);
+                    continue;
+                }
+
+                if let None = borrower_list.get(&to_t) {
+                    println!("[TRANSFER] TO borrower \"{}\" not found!", to_t);
+                    continue;
+                }
+
+                if let (Some(item), Some(from_b), Some(to_b)) = (
+                    items_list.get_mut(&item_t),
+                    borrower_list.get(&from_t),
+                    borrower_list.get(&to_t),
+                ) {
+                    let item_name = &item.name.clone();
+                    let (old_from_b_w, new_b_w) = item.transfer(from_b, to_b);
+
+                    if let Some(_) = new_b_w {
+                        println!(
+                            "[TRANSFER] Item \"{}\" transfered from \"{}\" to \"{}\"!",
+                            item.name, from_b.name, to_b.name
+                        );
+                    } else if let Some(old_from_b) = old_from_b_w {
+                        if from_b.name == old_from_b.name {
+                            println!(
+                                "[TRANSFER] Item \"{}\" is already borrowed by requester \"{}\"!",
+                                item.name, to_b.name
+                            );
+                        } else {
+                            println!("[TRANSFER] Item \"{}\" cannot be transferred as it is currently borrowed by \"{}\", not \"{}\"!", item_name, old_from_b.name, from_b.name);
+                        }
+                    } else {
+                        println!(
+                            "[TRANSFER] Item \"{}\" does not have a borrower!",
+                            item.name
+                        );
+                    }
+                }
+            }
+            "b" => {
+                let split_subcmd: Vec<&str> = subcmd.splitn(2, ' ').collect();
+                let item_t: String = split_subcmd[0].trim().to_string();
+                let borrower_t: String = split_subcmd[1].trim().to_string();
+
+                if let None = items_list.get(&item_t) {
+                    println!("[BORROW] Item \"{}\" not found!", item_t);
+                    continue;
+                }
+
+                if let None = borrower_list.get(&borrower_t) {
+                    println!("[BORROW] Borrower \"{}\" not found!", borrower_t);
+                    continue;
+                }
+
+                if let (Some(item), Some(borrower1)) =
+                    (items_list.get_mut(&item_t), borrower_list.get(&borrower_t))
+                {
+                    let item_name = &item.name.clone();
+                    if let Some(old_borrower) = item.borrow(&borrower1) {
+                        if old_borrower.name == borrower1.name {
+                            println!("[BORROW] Item \"{}\" already borrowed by requester and current borrower \"{}\"!", item.name, borrower1.name);
+                        } else {
+                            println!("[BORROW] Item \"{}\" cannot be borrowed by \"{}\" as it is currently borrowed by \"{}\"!", item_name, borrower1.name, old_borrower.name);
+                        }
+                    } else {
+                        println!(
+                            "[BORROW] Item \"{}\" borrowed by \"{}\"!",
+                            item.name, borrower1.name
+                        );
+                    }
+                }
+            }
+            "u" => {
+                if let Some(item) = items_list.get_mut(&subcmd) {
+                    let item_name = &item.name.clone();
+                    if let Some(borrower) = item.unborrow() {
+                        println!(
+                            "[UNBORROW] Item \"{}\" unborrowed from \"{}\"!",
+                            item_name, borrower.name
+                        );
+                    } else {
+                        println!("[UNBORROW] Item \"{}\" has no borrower!", item.name);
+                    }
+                } else {
+                    println!("[UNBORROW] Item \"{}\" not found!", subcmd);
+                }
+            }
+            _ => {}
+        }
     }
 }
