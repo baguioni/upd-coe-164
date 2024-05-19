af34f453a0a6dafd9eabab68ca1de31fd6f008f3
Vincent Alec F. Baguio <vfbaguio@up.edu.ph>
2024-05-20 03:27:07 +0800

    add week 4

---
+++ src/bin/w04/main.rs
@@ -0,0 +1,14 @@
+use std::collections::BinaryHeap;
+use std::ops::Deref;
+use std::io;
+
+#[derive(Debug, Ord, PartialOrd, PartialEq, Eq)]
+struct Request<T> (T);
+
+impl <T> Deref for Request <T> {
+    // TODO
+}
+
+fn main(){
+    // TODO
+}
